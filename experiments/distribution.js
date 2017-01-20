e6f0de496229dcecbdd6b35dd62a9ad4037b9372
PaoloSarti <paolo.sarti93@gmail.com>
2017-01-20 18:56:53 +0100

    More information theory experiments

--- experiments/distribution.js
+++ experiments/distribution.js
@@ -24,7 +24,7 @@ function distribution(o){
  */
 function normalize(obj){
     var normalized = {}
-    var sum = Stream.from(obj).map(e=>e.value).sum()
+    var sum = Stream.values(obj).sum()
     for(let key in obj){
         normalized[key] = obj[key]/sum
     }
@@ -37,7 +37,7 @@ console.log('Discrete distribution: '+JSON.stringify(d))
 var s = Stream.generate(distribution(d))
 
 console.log('Example stream: '+s.toArray(20))
-var n = 1000000
+var n = 100000
 console.log('n: '+n)
 var counts = s.take(n).reduce((o,e)=>{
     var inc = {}
@@ -50,7 +50,8 @@ var normalized = normalize(counts)
 console.log('Frequencies: '+JSON.stringify(normalized)+'\n')
 
 /**
- * Calculates the entropy of a distribution object (sum of values = 1)
+ * Calculates the entropy of a distribution object or of a normalized array (sum of values = 1)
+ * -sum_j(p_j*log2(p_j))
  */
 function entropy(obj){
     return -Stream.values(obj)
@@ -59,7 +60,8 @@ function entropy(obj){
 }
 
 /**
- * Calculates the gini index of a distribution object (sum of values = 1)
+ * Calculates the gini index of a distribution object or of a normalized array (sum of values = 1)
+ * 1-sum_j(p_j^2)
  */
 function giniIndex(obj){
     return 1-Stream.values(obj)
@@ -68,20 +70,36 @@ function giniIndex(obj){
 }
 
 /**
- * Calculates the misclassification error of a distribution object (sum of values = 1)
+ * Calculates the misclassification error of a distribution object or of a normalized array (sum of values = 1)
  */
 function misclassificationError(obj){
     return 1-Stream.values(obj).max()
 }
 
-console.log('Theoretiacal entropy: '+entropy(d))
+var uniform = {A:0.25,B:0.25,C:0.25,D:0.25}
+
+console.log('Entropy of '+JSON.stringify(uniform)+": "+entropy(uniform))
+console.log('Theoretiacal entropy of '+JSON.stringify(d)+': '+entropy(d))
 console.log('Tested entropy: '+entropy(normalized)+'\n')
 
-console.log('Theoretiacal Gini index: '+giniIndex(d))
+
+console.log('Gini index of '+JSON.stringify(uniform)+": "+giniIndex(uniform))
+console.log('Theoretiacal Gini index of '+JSON.stringify(d)+': '+giniIndex(d))
 console.log('Tested Gini index: '+giniIndex(normalized)+'\n')
 
-console.log('Theoretiacal misclassification error: '+misclassificationError(d))
+console.log('misclassification error of '+JSON.stringify(uniform)+": "+misclassificationError(uniform))
+console.log('Theoretiacal misclassification error of '+JSON.stringify(d)+': '+misclassificationError(d))
 console.log('Tested misclassification error: '+misclassificationError(normalized)+'\n')
+
+var distr = distribution(d)
+/*console.log(s.map(e=>e===distr()?1:0).take(n).reduce((a,b)=>{
+    var o = Object.assign({},a)
+    o.s+=b
+    o.n++
+    return o
+},{s:0,n:0}))*/
+console.log('Success rate of a random classifier: '+s.map(e=>e===distr()?1:0).take(n).avg())
+console.log('It should be 1-giniIndex: '+(1-giniIndex(d)))
 
 /**
  * A function that takes an encoding object (key=>value),
@@ -92,7 +110,7 @@ function encodeFn(enc){
         return enc[e]
     }
 }
-
+console.log('Encoding Test of '+JSON.stringify(d)+'\n')
 
 var naiveEncoding = {A:[0,0],B:[0,1],C:[1,0],D:[1,1]}
 console.log('naiveEncoding: '+JSON.stringify(naiveEncoding))
@@ -100,4 +118,35 @@ console.log('Size of encoded stream from '+n+' symbols: '+s.map(encodeFn(naiveEn
 
 var smartEncoding = {A:[0],B:[1,0],C:[1,1,0],D:[1,1,1]}
 console.log('smartEncoding: '+JSON.stringify(smartEncoding))
-console.log('Size of encoded stream from '+n+' symbols: '+s.map(encodeFn(smartEncoding)).take(n).flatten().size())
+console.log('Size of encoded stream from '+n+' symbols: '+s.map(encodeFn(smartEncoding)).take(n).flatten().size()+'\n')
+
+/**
+ * From {A:[0,0],B:[0,1],C:[1,0],D:[1,1]}, gives {"00":"A","01":"B","10":C,"11":D}
+ */
+function decodeObj(obj){
+    return Stream.from(obj)
+        .map(e=>{
+            var o = {}
+            var joined = e.value.join('')
+            o[joined]=e.name
+            return o })
+        .reduce(Object.assign,{})
+}
+
+var decoded = decodeObj(naiveEncoding)
+console.log('Decode '+JSON.stringify(naiveEncoding)+': '+JSON.stringify(decoded))
+
+/**
+ * returns a stream, decoded with decObj, assuming a fixed encoding length
+ */
+function decodeNaive(stream,decObj){
+    var l
+    for(var key in decObj){
+        l = key.length
+        break
+    }
+    return stream.buffer(l).map(e=>decObj[e.join('')])
+}
+
+var decodedStream = decodeNaive(s.map(encodeFn(naiveEncoding)).flatten(),decoded)
+console.log('Decoded naive: '+decodedStream.take(10).toArray())
