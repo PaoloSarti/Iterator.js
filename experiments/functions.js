2cb144bab25458efa9abec05fc97b8dfa3aeb444
PaoloSarti <paolo.sarti93@gmail.com>
2017-05-18 13:13:49 +0200

    More function examples

--- experiments/functions.js
+++ experiments/functions.js
@@ -9,4 +9,13 @@ console.log(Iterator(a).map(rectify).toArray())
 console.log(Iterator(a).map(softplus).toArray())
 console.log(Iterator.random().take(1000).groupBy(i=>i>0.5, 'count'))
 
-var factorials = ()=>Iter.range().skip(1).cumulate((a,b)=>a*b)
+//Some functions that can be easily expressed with Iterators:
+const factorials = ()=>Iter.range().skip(1).cumulate((a,b)=>a*b)
+
+const factorial = n=>Iter.range(2,n+1).product()
+
+const binomial = (n,k)=>Iter.range(1,k+1).map(i=>(n+1-i)/i).product()
+
+const binomials = n=>Iter.range(0,n+1).map(i=>binomial(n,i)).toArray()
+
+const binomialsPyramid = n=>Iter.range(0,n).map(binomials).toArray()
